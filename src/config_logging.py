4769baa55f19f1e281a1c5a1440bbc8d5ca69bc0
Igor Konov <igorkonov1993@gmail.com>
2025-10-19 21:04:39 +0300

    fix config logging

--- src/config_logging.py
+++ src/config_logging.py
@@ -1,9 +1,18 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
 
-logger = logging.getLogger(__name__)
+def setup_logging():
+    """Настраивает логирование для приложения."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        force=True,
+    )
+
+
+def get_logger(name):
+    """
+    Возвращает экземпляр логгера с заданным именем.
+    """
+    return logging.getLogger(name)
